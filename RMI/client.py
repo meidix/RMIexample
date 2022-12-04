e056b89053e9f86a43fdacbcb292619fe236d9bd
meidix <mahdihossieni@gmail.com>
2022-12-04 10:43:42 +0330

    did some small refactoring, I hope I have'nt broken anything

--- RMI/client.py
+++ RMI/client.py
@@ -2,12 +2,6 @@ import socket
 import pickle
 
 
-def invoke_remote_method(data, host, port):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.connect((host, port))
-        sock.sendall(pickle.dumps(data))
-        result = sock.recv(1024)
-        return pickle.loads(result)
 
 
 def serialize(function_name, args):
--- app/client/stub.py
+++ app/client/stub.py
@@ -1,6 +1,7 @@
 from ..common import ConferenceManagerInterface
-from RMI.client import serialize, invoke_remote_method
+from RMI.client import serialize
 from RMI import SERVER_HOST, SERVER_PORT
+from RMI.utils import send_socket_request
 
 
 class ConferenceManager(ConferenceManagerInterface):
@@ -8,11 +9,11 @@ class ConferenceManager(ConferenceManagerInterface):
     @classmethod
     def manager(cls):
         data = serialize('manager', None)
-        return invoke_remote_method(data, SERVER_HOST, SERVER_PORT)
+        return send_socket_request(data, SERVER_HOST, SERVER_PORT)
 
     def register(self, **kwargs):
         data = serialize('register', kwargs)        # send the data to server
-        return invoke_remote_method(data, SERVER_HOST, SERVER_PORT)
+        return send_socket_request(data, SERVER_HOST, SERVER_PORT)
 
     def buy_ticket(self, conference_index, name, age):
         data = serialize('buy_ticket', {
@@ -20,19 +21,19 @@ class ConferenceManager(ConferenceManagerInterface):
             'name': name,
             'age': age
         })        # send data to server
-        return invoke_remote_method(data, SERVER_HOST, SERVER_PORT)
+        return send_socket_request(data, SERVER_HOST, SERVER_PORT)
 
     def all(self):
         data = {
             'function_name': 'all',
             'args': None
         }
-        return invoke_remote_method(data, SERVER_HOST, SERVER_PORT)
+        return send_socket_request(data, SERVER_HOST, SERVER_PORT)
 
     def latest(self):
         data = serialize('latest', None)
-        return invoke_remote_method(data, SERVER_HOST, SERVER_PORT)
+        return send_socket_request(data, SERVER_HOST, SERVER_PORT)
 
     def closest(self):
         data = serialize('closest', None)
-        return invoke_remote_method(data, SERVER_HOST, SERVER_PORT)
+        return send_socket_request(data, SERVER_HOST, SERVER_PORT)
